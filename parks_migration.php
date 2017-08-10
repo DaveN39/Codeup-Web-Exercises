eff40fec5b03a378e2c282f78c807142f40428c3
David <david.newton39@yahoo.com>
2017-08-10 17:10:44 -0500

    DB_Connect, Parks_Logins, Parks_Migration, Parks_Seeder and PDO_Test exercises for PHP IV

---
+++ parks_migration.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "db_connect.php";
+$createTableQuery = 'CREATE TABLE national_parks(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(150) NOT NULL,
+    location VARCHAR(165) NOT NULL,
+    date_established DATE NOT NULL,
+    area_in_acres DOUBLE NOT NULL,
+    description TEXT,
+    PRIMARY KEY (id)
+)';
+$dbc->exec('DROP TABLE IF EXISTS national_parks');
+$dbc->exec($createTableQuery);
---
+++ pdo_test.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'codeup_test_db');
+define('DB_USER', 'vagrant');
+define('DB_PASS', 'vagrant');
+
+// require_once 'db_connect.php';
+// echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . PHP_EOL;
